7f18390b8dd1b7a869c4ade7fe8c0582c4efbfe4
SrPerso <carlos@jardiper.es>
2017-09-23 18:24:45 +0200

    improved math test

--- MC Engine/ModuleUI.cpp
+++ MC Engine/ModuleUI.cpp
@@ -25,8 +25,8 @@ bool ModuleUI::Start()
 
 	//Windows ;
 
-
-	openConsoleW = true;
+	OpenTeamInfoW = false;
+	openConsoleW = false;
 
 
 	return ret;
@@ -43,7 +43,9 @@ update_status ModuleUI::Update(float dt)
 
 
 	static bool show_test_window = false;
-
+	static bool show_Console_window = false;
+	static bool show_Configuration_window = false;
+	static bool show_MathTest_window = false;
 
 	if (show_test_window)
 	{
@@ -57,6 +59,7 @@ update_status ModuleUI::Update(float dt)
 		
 		if (ImGui::BeginMenu("File"))
 		{
+
 			if (ImGui::MenuItem("Quit", "ESC"))
 					return UPDATE_STOP;			
 
@@ -65,17 +68,17 @@ update_status ModuleUI::Update(float dt)
 
 		if (ImGui::BeginMenu("View"))
 		{
-
-
+			
 			ImGui::Checkbox("test window", &show_test_window);
 
 
-			if (ImGui::MenuItem("Console"))
+			if (ImGui::Checkbox("Console", &show_Console_window))
 			{
-
+				openConsoleW = !openConsoleW;
+				consoleActive = !consoleActive;
 			}
 
-			if (ImGui::MenuItem("Configuration"))
+			if (ImGui::Checkbox("Configuration", &show_Configuration_window))
 			{
 
 			}
@@ -85,10 +88,38 @@ update_status ModuleUI::Update(float dt)
 
 
 
-		if (ImGui::BeginMenu("Team"))
+		if (ImGui::BeginMenu("Help"))
 		{
-			ImGui::Checkbox("Team", &show_test_window);
-			ImGui::Checkbox("Project", &show_test_window);
+			if (ImGui::MenuItem("Team"))
+			{
+				OpenTeamInfoW = !OpenTeamInfoW;
+				TeamInfoActive = !TeamInfoActive;
+			}
+
+			if (ImGui::BeginMenu("About MC Engine..."))
+			{					
+
+				if (ImGui::MenuItem("Github repository"))
+				{
+					ShellExecuteA(NULL, "open", "https://github.com/SrPerso/MC-ENGINE-", NULL, NULL, SW_SHOWNORMAL);
+				}
+			
+				if (ImGui::MenuItem("Wiki"))
+				{
+					ShellExecuteA(NULL, "open", "https://github.com/SrPerso/MC-ENGINE-/wiki", NULL, NULL, SW_SHOWNORMAL);
+				}
+		
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Latest Release"))
+			{
+				ShellExecuteA(NULL, "open", "https://github.com/SrPerso/MC-ENGINE-/releases", NULL, NULL, SW_SHOWNORMAL);
+			}
+			if (ImGui::MenuItem("Report a bug"))
+			{
+				ShellExecuteA(NULL, "open", "https://github.com/SrPerso/MC-ENGINE-/issues", NULL, NULL, SW_SHOWNORMAL);
+			}
+
 			ImGui::EndMenu();
 		}
 
@@ -96,85 +127,105 @@ update_status ModuleUI::Update(float dt)
 
 		if (ImGui::BeginMenu("Window"))
 		{
+			if (ImGui::Checkbox("MathTest",&show_MathTest_window)) {
+
+
+
+			}
+
+		/*	if (ImGui::BeginMenu("MathTest"))
+			{
+				ImGui::Text("Sphere1 (1,1,0) with 2 radius and Sphere2(2,2,0) with radius 1");
+
+				math::Sphere sphere1(float3(1.0f, 1.0f, 0.0f), 2);
+				math::Sphere sphere2(float3(2.0f, 2.0f, 0.0f), 1);
+
+				intersects = sphere1.Intersects(sphere2);
+
+
+				if (intersects)
+				{
+					ImGui::Text("True");
+				}
+				else
+				{
+					ImGui::Text("False");
+				}
+
+				ImGui::Text("Sphere3 (0,0,0) with 1 radius and Sphere4(2,2,0) with radius 1");
+
+				math::Sphere sphere3(float3(0.0f, 0.0f, 0.0f), 1);
+				math::Sphere sphere4(float3(2.0f, 2.0f, 0.0f), 1);
+
+				intersects = sphere3.Intersects(sphere4);
+
+
+				if (intersects)
+				{
+					ImGui::Text("True");
+				}
+				else
+				{
+					ImGui::Text("False");
+				}
+
+
+				ImGui::Text("Sphere5 (1,1,0) with 2 radius and Capsule1(2,2,0) with radius 1");
+
+				math::Sphere sphere5(float3(1.0f, 1.0f, 0.0f), 2);
+				math::Capsule capsule1(float3(2.0f, 2.0f, 0.0f), float3(2.0f, 3.0f, 0.0f), 1);
+
+				intersects = sphere5.Intersects(capsule1);
+
+
+				if (intersects)
+				{
+					ImGui::Text("True");
+				}
+				else
+				{
+					ImGui::Text("False");
+				}
+
+				ImGui::Text("Sphere6 (1,1,0) with 2 radius and Capsule2(10,20,0) with radius 1");
+
+				math::Sphere sphere6(float3(1.0f, 1.0f, 0.0f), 2);
+				math::Capsule capsule2(float3(10.0f, 20.0f, 0.0f), float3(10.0f, 23.0f, 0.0f), 1);
+
+				intersects = sphere6.Intersects(capsule2);
+
+
+				if (intersects)
+				{
+					ImGui::Text("True");
+				}
+				else
+				{
+					ImGui::Text("False");
+				}
+				ImGui::EndMenu();
+			}*/
+
+
 			ImGui::EndMenu();
 		}
-
-		if (ImGui::BeginMenu("MathTest"))
-		{
-			ImGui::Text("Sphere1 (1,1,0) with 2 radius and Sphere2(2,2,0) with radius 1");
-
-			math::Sphere sphere1(float3(1.0f, 1.0f, 0.0f), 2);
-			math::Sphere sphere2(float3(2.0f, 2.0f, 0.0f), 1);
-
-			intersects = sphere1.Intersects(sphere2);
-
-
-			if (intersects)
-			{
-				ImGui::Text("True");
-			}
-			else
-			{
-				ImGui::Text("False");
-			}
-
-			ImGui::Text("Sphere3 (0,0,0) with 1 radius and Sphere4(2,2,0) with radius 1");
-
-			math::Sphere sphere3(float3(0.0f, 0.0f, 0.0f), 1);
-			math::Sphere sphere4(float3(2.0f, 2.0f, 0.0f), 1);
-
-			intersects = sphere3.Intersects(sphere4);
-
-
-			if (intersects)
-			{
-				ImGui::Text("True");
-			}
-			else
-			{
-				ImGui::Text("False");
-			}
-
-
-			ImGui::Text("Sphere5 (1,1,0) with 2 radius and Capsule1(2,2,0) with radius 1");
-
-			math::Sphere sphere5(float3(1.0f, 1.0f, 0.0f), 2);
-			math::Capsule capsule1(float3(2.0f, 2.0f, 0.0f), float3(2.0f, 3.0f, 0.0f), 1);
-
-			intersects = sphere5.Intersects(capsule1);
-
-
-			if (intersects)
-			{
-				ImGui::Text("True");
-			}
-			else
-			{
-				ImGui::Text("False");
-			}
-
-			ImGui::Text("Sphere6 (1,1,0) with 2 radius and Capsule2(10,20,0) with radius 1");
-
-			math::Sphere sphere6(float3(1.0f, 1.0f, 0.0f), 2);
-			math::Capsule capsule2(float3(10.0f, 20.0f, 0.0f), float3(10.0f, 23.0f, 0.0f), 1);
-
-			intersects = sphere6.Intersects(capsule2);
-
-
-			if (intersects)
-			{
-				ImGui::Text("True");
-			}
-			else
-			{
-				ImGui::Text("False");
-			}
-			ImGui::EndMenu();
-		}
-
+			
 		ImGui::EndMainMenuBar();
-
 	}
+
+
+
+
+	if (show_MathTest_window)
+		ShowMathWindow();
+
+	if (consoleActive)
+		ShowConsoleWindow();
+
+	if (TeamInfoActive)
+		ShowTeamInfoWindow();
+
+
 
 	ImGui::Render();
 
@@ -220,6 +271,166 @@ IMGUI_API void ModuleUI::ShowConsoleWindow(bool * p_open)
 
 	ImGui::End();
 
+	return IMGUI_API void();
+}
+
+IMGUI_API void ModuleUI::ShowTeamInfoWindow(bool * p_open)
+{
+	ImGuiWindowFlags window_flags = 0;
+
+	if (!ImGui::Begin("Team", p_open, window_flags))
+	{
+		// Early out if the window is collapsed, as an optimization.
+		ImGui::End();
+
+		return;
+	}
+
+	//ImGui::PushItemWidth(ImGui::GetWindowWidth() * 0.65f);    // 2/3 of the space for widget and 1/3 for labels
+	ImGui::PushItemWidth(-140);                                 // Right align, keep 140 pixels for labels
+
+						
+										//ImGui::Text("%s", consoleText);
+	ImGui::Text("This engine is made by:\n ");
+	ImGui::Text("\t Marc Samper Tarancon and Carlos Peralta  ");
+	ImGui::Text("\n\n\n");
+	ImGui::Text("You can found us on github: \n\n");
+
+
+	if (ImGui::MenuItem(" - Carlos"))
+	{
+		ShellExecuteA(NULL, "open", "https://github.com/SrPerso/", NULL, NULL, SW_SHOWNORMAL);
+	}
+	if (ImGui::MenuItem(" - Marc"))
+	{
+		ShellExecuteA(NULL, "open", "https://github.com/marcsamper", NULL, NULL, SW_SHOWNORMAL);
+	}
+
+	ImGui::End();
+
+	return IMGUI_API void();
+}
+
+IMGUI_API void ModuleUI::ShowMathWindow(bool * p_open)
+{
+	// Demonstrate the various window flags. Typically you would just use the default.
+	ImGuiWindowFlags window_flags = 0;
+
+	if (!ImGui::Begin("Math Library Tests", p_open, window_flags))
+	{
+		// Early out if the window is collapsed, as an optimization.
+		ImGui::End();
+		return;
+	}
+
+	static bool intersects= false;
+	static bool intersectsTrue = false;
+	static bool intersectsFalse = false;
+
+	static bool sphere1selected= false;
+	static bool sphere2selected = false;
+	static bool Capsuleselected = false;
+
+
+	ImGui::Checkbox("Sphere1", &sphere1selected);
+	ImGui::Checkbox("Sphere2", &sphere2selected);
+	ImGui::Checkbox("Capsule1", &Capsuleselected);
+
+
+	if (sphere1selected){
+		ImGui::Text("Sphere 1:");
+		ImGui::InputInt("Radius", &sphere1.sphereRadius);
+		ImGui::InputInt("X", &sphere1.sphereX);
+		ImGui::InputInt("Y", &sphere1.sphereY);
+		ImGui::InputInt("Z", &sphere1.sphereZ);
+	}
+	if (sphere2selected) {
+		ImGui::Text("Sphere 2:");
+		ImGui::InputInt("Radius 2", &sphere2.sphereRadius);
+		ImGui::InputInt("X 2", &sphere2.sphereX);
+		ImGui::InputInt("Y 2", &sphere2.sphereY);
+		ImGui::InputInt("Z 2", &sphere2.sphereZ);
+	}
+
+	if (Capsuleselected) {
+		ImGui::Text("Capsule 1:");
+		ImGui::InputInt("Capsule 1 Radius", &capsule1.capsuleRadius);
+		ImGui::InputInt("Bottom X", &capsule1.capsuleBotX);
+		ImGui::InputInt("Bottom Y", &capsule1.capsuleBotY);
+		ImGui::InputInt("Bottom Z", &capsule1.capsuleBotZ);
+		ImGui::InputInt("Top X", &capsule1.capsuleTopX);
+		ImGui::InputInt("Top Y", &capsule1.capsuleTopY);
+		ImGui::InputInt("Top Z", &capsule1.capsuleTopZ);
+	}
+	
+	if (ImGui::Button("Run Math Test"))
+	{
+		if (sphere1selected && sphere2selected && Capsuleselected) 
+			ImGui::Text("it can be done.. Too many selections");
+
+		else if (!sphere1selected && !sphere2selected || !sphere2selected && !Capsuleselected)
+			ImGui::Text("it can be done.. is necesary 2 selections");
+
+		else if (sphere1selected && sphere2selected) 
+		{
+			math::Sphere sphere1(float3(sphere1.sphereX, sphere1.sphereY, sphere1.sphereZ), sphere1.sphereRadius);
+			math::Sphere sphere2(float3(sphere2.sphereX, sphere2.sphereY, sphere2.sphereZ), sphere2.sphereRadius);
+
+			intersects = sphere1.Intersects(sphere2);
+		}
+
+		else if (sphere1selected && Capsuleselected) 
+		{
+
+			math::Sphere sphere1(float3(sphere1.sphereX, sphere1.sphereY, sphere1.sphereZ), sphere1.sphereRadius);
+			math::Capsule capsule1(float3(capsule1.capsuleBotX, capsule1.capsuleBotY, capsule1.capsuleBotZ), float3(capsule1.capsuleTopX, capsule1.capsuleTopY, capsule1.capsuleTopZ), capsule1.capsuleRadius);
+
+
+			intersects = sphere1.Intersects(capsule1);
+		}
+
+		else if (sphere1selected && sphere2selected)
+		{
+
+			math::Capsule capsule1(float3(capsule1.capsuleBotX, capsule1.capsuleBotY, capsule1.capsuleBotZ), float3(capsule1.capsuleTopX, capsule1.capsuleTopY, capsule1.capsuleTopZ), capsule1.capsuleRadius);
+			math::Sphere sphere2(float3(sphere2.sphereX, sphere2.sphereY, sphere2.sphereZ), sphere2.sphereRadius);
+
+			intersects = capsule1.Intersects(sphere2);
+		}
+
+		if (intersects)
+		{
+			intersectsTrue = true;
+			intersectsFalse = false;
+		}
+		else
+		{
+			intersectsTrue = false;
+			intersectsFalse = true;
+		}
+	}
+
+	ImGui::Text("Intersection:");
+
+	if (intersectsTrue)
+		ImGui::Text("\t True");
+		
+	if (intersectsFalse)
+		ImGui::Text("\t False");
+		
+	
+	if (ImGui::Button("Reset"))
+	{
+		intersects = false;
+		intersectsTrue = false;
+		intersectsFalse = false;
+		sphere1.~sphereTest();
+		sphere2.~sphereTest();
+		capsule1.~capsuleTest();
+	}
+
+
+		ImGui::End();
 	return IMGUI_API void();
 }
 
--- MC Engine/ModuleUI.h
+++ MC Engine/ModuleUI.h
@@ -6,6 +6,63 @@
 #include "imGUI\imgui.h"
 #include <string>
 #include <vector>
+
+
+template<class TYPE>
+struct sphereTest {
+	
+	sphereTest() {
+		sphereRadius = 0;
+		sphereX = 0;
+		sphereY = 0;
+		sphereZ = 0;
+	}
+	~sphereTest() {
+		sphereRadius = 0;
+		sphereX = 0;
+		sphereY = 0;
+		sphereZ = 0;
+	}
+
+	TYPE sphereRadius = 0;
+	TYPE sphereX = 0;
+	TYPE sphereY = 0;
+	TYPE sphereZ = 0;
+
+};
+
+template<class TYPE>
+struct capsuleTest{
+
+	capsuleTest() {
+		capsuleRadius = 0;
+		capsuleBotX = 0;
+		capsuleBotY = 0;
+		capsuleBotZ = 0;
+		capsuleTopX = 0;
+		capsuleTopY = 0;
+		capsuleTopZ = 0;
+	}	
+	~capsuleTest() {
+		capsuleRadius = 0;
+		capsuleBotX = 0;
+		capsuleBotY = 0;
+		capsuleBotZ = 0;
+		capsuleTopX = 0;
+		capsuleTopY = 0;
+		capsuleTopZ = 0;
+	}
+
+	TYPE capsuleRadius = 0;
+	TYPE capsuleBotX = 0;
+	TYPE capsuleBotY = 0;
+	TYPE capsuleBotZ = 0;
+	TYPE capsuleTopX = 0;
+	TYPE capsuleTopY = 0;
+	TYPE capsuleTopZ = 0;
+};
+
+
 class ModuleUI : public Module
 {
 public:
@@ -18,6 +75,8 @@ public:
 	bool CleanUp();
 public:
 	IMGUI_API void ShowConsoleWindow(bool* p_open = NULL);
+	IMGUI_API void ShowTeamInfoWindow(bool* p_open = NULL);
+	IMGUI_API void ShowMathWindow(bool* p_open = NULL);
 	void AddLogToConsole(std::string toAdd);
 
 public://math
@@ -25,9 +84,24 @@ public://math
 
 private:// open Windows
 	bool openConsoleW;
-
+	bool OpenTeamInfoW;
+	bool OpenMathW;
+private: // Active
+	bool TeamInfoActive = false;
+	bool consoleActive = false;
+	bool MathTestActive = false;
 private:
 	std::vector<std::string> consoleTxt;
+
+
+
+//---------Math
+private:
+	//future actualitation : Create a object builder
+	sphereTest<int> sphere1;
+	sphereTest<int> sphere2;
+	capsuleTest<int> capsule1;
+
 
 };
 
